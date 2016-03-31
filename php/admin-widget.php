e5dc76e0f73e9813914a0a56a52b1577d20f3061
Joe Querin <joecue@gmail.com>
2016-03-31 19:59:27 -0400

    Changed branding to WD2

--- php/admin-widget.php
+++ php/admin-widget.php
@@ -2,12 +2,12 @@
 
 //LCCC Dashboard Widget
 
-add_action('wp_dashboard_setup', 'lccc_admin_dashboard_widgets');
+add_action('wp_dashboard_setup', 'wd2_admin_dashboard_widgets');
  
-function lccc_admin_dashboard_widgets() {
+function wd2_admin_dashboard_widgets() {
 global $wp_meta_boxes;
 
-wp_add_dashboard_widget('lccc_help_widget', 'Web Site Support', 'lccc_help_widget');
+wp_add_dashboard_widget('wd2_help_widget', 'Web Site Support', 'wd2_help_widget');
 
  	// Globalize the metaboxes array, this holds all the widgets for wp-admin
  
@@ -20,20 +20,20 @@ wp_add_dashboard_widget('lccc_help_widget', 'Web Site Support', 'lccc_help_widge
  	
  	// Backup and delete our new dashboard widget from the end of the array
 	
- 	$lccc_widget_backup = array( 'lccc_help_widget' => $normal_dashboard['lccc_help_widget'] );
- 	unset( $normal_dashboard['lccc_help_widget'] );
+ 	$wd2_widget_backup = array( 'wd2_help_widget' => $normal_dashboard['wd2_help_widget'] );
+ 	unset( $normal_dashboard['wd2_help_widget'] );
  
  	// Merge the two arrays together so our widget is at the beginning
  
- 	$sorted_dashboard = array_merge( $lccc_widget_backup, $normal_dashboard );
+ 	$sorted_dashboard = array_merge( $wd2_widget_backup, $normal_dashboard );
  
  	// Save the sorted array back into the original metaboxes 
  
  	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
 }
 
-function lccc_help_widget() {
-echo '<h1>Welcome to LCCC Proud!</h1>
+function wd2_help_widget() {
+echo '<h1>Welcome to Web Design 2!</h1>
 <p><strong>Need help? <br />Please contact either</strong>
 <ul style="list-style: disc; margin: 0 0 0 40px;">
 <li> <strong>Joe Querin</strong> at <a href="mailto:jquerin@lorainccc.edu">jquerin@lorainccc.edu</a> or ext 7060.</li>
